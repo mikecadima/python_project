72e7b89daf8bc77ba7cdbc42c98734eb8a8bb507
mikecadima <65040522+mikecadima@users.noreply.github.com>
2020-06-13 09:39:36 -0400

    Update pet_game.py

--- pet_game.py
+++ pet_game.py
@@ -1,98 +1,65 @@
 #my game-virtual pet
-'''
-dog = {
-    "name": "Bane",
-    "Stomach": 100,
-    "Joy": 50,
-}
-
-# function to feed pet
-def feed_pet(pet):
-    pet["Stomach"] += 10
-
-def play_with_pet(pet):
-    pet["Joy"] += 10
-
-# Decrease pet levels.    
-def hungry_and_lonely(pet):
-    pet["Stomach"] -= 5
-    pet["Joy"] -= 5
-
-# Input for user to interact with pet
-while True:
-    print("Welcome to Virtual Pet!")
-    print("""
-Your pet %s's levels:
-Stomach: %d
-Joy: %d
-""" % (dog["name"], dog["Stomach"], dog["Joy"]))
-    
-    choice = int(input("""
-1. Feed the dog
-2. Play with the dog
-3. Don't do either one
-"""))  
-   
-
-    if choice == 1:
-        feed_pet(dog)
-    elif choice == 2:
-        play_with_pet(dog)
-    elif choice == 3:
-        print("Thank you for playing!")
-        exit()
-
-    # loop will run, it will need an input or prompted to end game
-    hungry_and_lonely(dog)
-'''
 class Pet:
-    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+    def __init__(self, name, fullness=50, happiness=50, hunger=50, sadness=50):
         self.name = name
         self.fullness = fullness
         self.happiness = happiness
         self.hunger = hunger
-        self.mopiness = mopiness
+        self.sadness = sadness
         self.toys = []
         self.treats = []
 
     def eat_food(self):
-        self.fullness += 30
+        self.fullness += 20
+        self.hunger -= 5
 
     def get_love(self):
-        self.happiness += 30
+        self.happiness += 20
+        self.sadness -= 5
 
     def be_alive(self):
         self.fullness -= self.hunger
         self.happiness -= self.mopiness
-        for toy in self.toys:
-            self.happiness += toy.use()
 
-    def reward_snack(self):
-        self.fullness -= self.hunger
-        self.happiness -= self.mopiness        
-        for treat in self.treats:
-            self.happiness += treat.snack()
+    def get_toy(self):
+        self.fullness += 10
+        self.happiness += 10
+        self.sadness -= 10
+
+    def pizza(self):
+        self.fullness += 20
+        self.happiness += 20
+        self.hunger -= 5
+        self.sadness -= 5
+
+    def bacon(self):
+        self.fullness += 20
+        self.happiness += 20
+        self.hunger -= 5
+        self.sadness -= 5
+
+    def vegan_snack(self):
+        self.fullness += 5
+        self.happiness += 5
+        self.hunger -= 5
+        self.sadness -= 2
 
     def __str__(self):
         return """
-        %s:
+        \033[34m %s: \033[0m
         Fullness: %d
         Happiness: %d
-        """ % (self.name, self.fullness, self.happiness)
-
-    def get_toy(self, toy):
-        self.toys.append(toy)
-
-    def get_treat(self, treat):
-        self.treats.append(treat)
+        Hunger: %d
+        Sadness: %d
+        """ % (self.name, self.fullness, self.happiness, self.hunger, self.sadness)
 
 class CuddlyPet(Pet):
-    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, sadness=5):
         super(CuddlyPet,self).__init__(name, fullness,100,hunger,1)
         self.cuddle_level = cuddle_level
     def be_alive(self):
         self.fullness -= self.hunger
-        self.happiness -= self.mopiness/2
+        self.happiness -= self.sadness/2
         for toy in self.toys:
             self.happiness += toy.use()
 
@@ -102,3 +69,6 @@ class CuddlyPet(Pet):
 
     def __str__(self):
         return "Extra cuddly"
+
+
+
